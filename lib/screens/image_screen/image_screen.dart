193007bbe1016b33219b78fb367ef0fd764d6e2d
SirusCodes <darshandrander@gmail.com>
2020-12-29 17:39:29 +0530

    show multiple screens for longer write-ups

--- lib/screens/image_screen/image_screen.dart
+++ lib/screens/image_screen/image_screen.dart
@@ -7,7 +7,7 @@ import 'widgets/image_color_handler.dart';
 import 'widgets/image_text_hander.dart';
 import 'widgets/poem_image_widget.dart';
 
-class ImageScreen extends ConsumerWidget {
+class ImageScreen extends StatelessWidget {
   const ImageScreen({
     Key key,
     @required this.poem,
@@ -20,40 +20,29 @@ class ImageScreen extends ConsumerWidget {
   final List<String> poem;
 
   @override
-  Widget build(BuildContext context, ScopedReader watch) {
-    final _textScale = watch(textSizeProvider).state;
-
+  Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              final List<String> poemLines = [];
+          child: Consumer(
+            builder: (context, watch, child) {
+              final _textScale = watch(textSizeProvider).state;
+              return LayoutBuilder(
+                builder: (context, constraints) {
+                  final List<List<String>> poemLines = _getPoemSeparated(
+                    context,
+                    constraints,
+                    _textScale,
+                  );
 
-              final _size = MediaQuery.of(context).size;
-              double _height = _size.height - 200;
-
-              // subtracting title height
-              _height -=
-                  _calcTextSize(context, constraints, title, 30, _textScale)
-                      .height;
-
-              // subtracting poet height
-              _height -=
-                  _calcTextSize(context, constraints, poet, 18, _textScale)
-                      .height;
-
-              // subtracting poem height
-              for (final line in poem) {
-                _height -=
-                    _calcTextSize(context, constraints, line, 15, _textScale)
-                        .height;
-                if (_height <= 0) break;
-
-                poemLines.add(line);
-              }
-
-              return PoemImageWidget(poem: poemLines);
+                  return PageView.builder(
+                    itemCount: poemLines.length,
+                    itemBuilder: (context, index) => PoemImageWidget(
+                      poem: poemLines[index],
+                    ),
+                  );
+                },
+              );
             },
           ),
         ),
@@ -86,6 +75,48 @@ class ImageScreen extends ConsumerWidget {
         onDonePressed: () {},
       ),
     );
+  }
+
+  List<List<String>> _getPoemSeparated(
+    BuildContext context,
+    BoxConstraints constraints,
+    double _textScale,
+  ) {
+    final List<List<String>> poemLines = [];
+    final List<String> poemLine = [];
+
+    final _size = MediaQuery.of(context).size;
+
+    // subtracting title height
+    final _titleHeight =
+        _calcTextSize(context, constraints, title, 30, _textScale).height;
+
+    // subtracting poet height
+    final _poetHeight =
+        _calcTextSize(context, constraints, poet, 18, _textScale).height;
+
+    final double _availableHeight =
+        _size.height - 200 - _titleHeight - _poetHeight;
+
+    double _height = _availableHeight;
+
+    // subtracting poem height
+    for (final line in poem) {
+      _height -=
+          _calcTextSize(context, constraints, line, 15, _textScale).height;
+
+      if (_height <= 0) {
+        poemLines.add([...poemLine]);
+        poemLine.clear();
+        _height = _availableHeight;
+      }
+
+      poemLine.add(line);
+    }
+
+    if (_height > 0) poemLines.add([...poemLine]);
+
+    return poemLines;
   }
 
   Size _calcTextSize(
