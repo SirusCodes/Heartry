8a434f8715f2cbf9b6186ce14a810e2e2d76e8b5
SirusCodes <darshandrander@gmail.com>
2021-01-19 18:18:24 +0530

    (feat): saving images to application app directory

--- lib/screens/image_screen/image_screen.dart
+++ lib/screens/image_screen/image_screen.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/all.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:screenshot/screenshot.dart';
+import 'package:share/share.dart';
 
 import '../../providers/text_providers.dart';
 import 'widgets/image_bottom_app_bar.dart';
@@ -7,7 +13,7 @@ import 'widgets/image_color_handler.dart';
 import 'widgets/image_text_hander.dart';
 import 'widgets/poem_image_widget.dart';
 
-class ImageScreen extends StatelessWidget {
+class ImageScreen extends StatefulWidget {
   const ImageScreen({
     Key key,
     @required this.poem,
@@ -20,6 +26,23 @@ class ImageScreen extends StatelessWidget {
   final List<String> poem;
 
   @override
+  _ImageScreenState createState() => _ImageScreenState();
+}
+
+class _ImageScreenState extends State<ImageScreen> {
+  final ScreenshotController _screenshot = ScreenshotController();
+
+  final PageController _pageController = PageController();
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  List<List<String>> poemLines;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
@@ -29,19 +52,23 @@ class ImageScreen extends StatelessWidget {
               final _textScale = watch(textSizeProvider).state;
               return LayoutBuilder(
                 builder: (context, constraints) {
-                  final List<List<String>> poemLines = _getPoemSeparated(
+                  poemLines = _getPoemSeparated(
                     context,
                     constraints,
                     _textScale,
                   );
 
-                  return PageView.builder(
-                    itemCount: poemLines.length,
-                    itemBuilder: (context, index) => PoemImageWidget(
-                      title: title,
-                      poem: poemLines[index],
-                      page: index,
-                      total: poemLines.length,
+                  return Screenshot(
+                    controller: _screenshot,
+                    child: PageView.builder(
+                      controller: _pageController,
+                      itemCount: poemLines.length,
+                      itemBuilder: (context, index) => PoemImageWidget(
+                        title: widget.title,
+                        poem: poemLines[index],
+                        page: index,
+                        total: poemLines.length,
+                      ),
                     ),
                   );
                 },
@@ -75,9 +102,53 @@ class ImageScreen extends StatelessWidget {
             },
           );
         },
-        onDonePressed: () {},
+        onDonePressed: () async {
+          final List<String> images = [];
+
+          showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (context) {
+              return const Dialog(
+                child: Material(
+                  child: Center(
+                    heightFactor: 3,
+                    child: CircularProgressIndicator(),
+                  ),
+                ),
+              );
+            },
+          );
+
+          for (int pageIndex = 0; pageIndex < poemLines.length; pageIndex++) {
+            _pageController.jumpToPage(pageIndex);
+            final path = await _getImage();
+            images.add(path);
+
+            print("Print: $path");
+          }
+
+          Navigator.pop(context);
+          Share.shareFiles(images);
+        },
       ),
     );
+  }
+
+  Future<String> _getImage() async {
+    final externalStorage = await getExternalStorageDirectory();
+
+    final path = join(
+      externalStorage.path,
+      "${DateTime.now().toIso8601String()}.png",
+    );
+
+    final File imgFile = await _screenshot.capture(
+      delay: const Duration(milliseconds: 50),
+      path: path,
+    );
+
+    return imgFile.path;
   }
 
   List<List<String>> _getPoemSeparated(
@@ -92,11 +163,12 @@ class ImageScreen extends StatelessWidget {
 
     // subtracting title height
     final _titleHeight =
-        _calcTextSize(context, constraints, title, 30, _textScale).height;
+        _calcTextSize(context, constraints, widget.title, 30, _textScale)
+            .height;
 
     // subtracting poet height
     final _poetHeight =
-        _calcTextSize(context, constraints, poet, 18, _textScale).height;
+        _calcTextSize(context, constraints, widget.poet, 18, _textScale).height;
 
     final double _availableHeight =
         _size.height - 200 - _titleHeight - _poetHeight;
@@ -104,7 +176,7 @@ class ImageScreen extends StatelessWidget {
     double _height = _availableHeight;
 
     // subtracting poem height
-    for (final line in poem) {
+    for (final line in widget.poem) {
       final double _heightToSub =
           _calcTextSize(context, constraints, line, 15, _textScale).height;
 
