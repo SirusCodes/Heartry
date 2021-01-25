ccc89e4e19d5adb2acbbd909ec6a190012a0e56f
SirusCodes <darshandrander@gmail.com>
2021-01-25 23:32:31 +0530

    saving image

--- lib/screens/profile_screen/profile_screen.dart
+++ lib/screens/profile_screen/profile_screen.dart
@@ -1,7 +1,13 @@
-import 'package:flutter/material.dart';
-import 'package:heartry/database/config.dart';
-import 'package:heartry/init_get_it.dart';
+import 'dart:io';
 
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+
+import '../../database/config.dart';
+import '../../init_get_it.dart';
 import '../../widgets/c_screen_title.dart';
 import '../../widgets/only_back_button_bottom_app_bar.dart';
 
@@ -15,10 +21,25 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   TextEditingController _nameController;
 
+  String _imagePath;
+  String _name;
+
+  final _config = locator<Config>();
+
   @override
   void initState() {
     super.initState();
-    _nameController = TextEditingController(text: locator<Config>().name);
+    _imagePath = _config.profile;
+    _name = _config.name;
+    _nameController = TextEditingController(text: _name);
+  }
+
+  @override
+  void dispose() {
+    if (_name != _nameController.text) _config.name = _nameController.text;
+
+    _nameController.dispose();
+    super.dispose();
   }
 
   @override
@@ -28,15 +49,27 @@ class _ProfileScreenState extends State<ProfileScreen> {
         child: ListView(
           children: <Widget>[
             const CScreenTitle(title: "Profile"),
-            const CircleAvatar(
-              maxRadius: 100,
-              minRadius: 80,
+            GestureDetector(
+              onTap: () => _setImage(),
+              child: CircleAvatar(
+                maxRadius: 100,
+                minRadius: 80,
+                backgroundImage:
+                    _imagePath != null ? FileImage(File(_imagePath)) : null,
+                child: _imagePath == null
+                    ? const Icon(
+                        Icons.person_add,
+                        size: 100,
+                      )
+                    : null,
+              ),
             ),
             const SizedBox(height: 10),
             Padding(
               padding: const EdgeInsets.all(15.0),
               child: TextField(
                 controller: _nameController,
+                textCapitalization: TextCapitalization.words,
                 decoration: const InputDecoration(
                   labelText: "Name",
                   border: OutlineInputBorder(),
@@ -46,15 +79,28 @@ class _ProfileScreenState extends State<ProfileScreen> {
           ],
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          // TODO Save data
-        },
-        label: const Text("Save"),
-        icon: const Icon(Icons.save),
-      ),
       bottomNavigationBar: const OnlyBackButtonBottomAppBar(),
     );
+  }
+
+  Future<void> _setImage() async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.getImage(source: ImageSource.gallery);
+
+    if (pickedImage == null) return;
+
+    final directory = await getApplicationDocumentsDirectory();
+    final fileExtension = p.extension(pickedImage.path);
+
+    final imageSaved = join(directory.path, "profile-picture$fileExtension");
+    final image = await File(imageSaved).writeAsBytes(
+      await pickedImage.readAsBytes(),
+    );
+
+    setState(() {
+      _imagePath = image.path;
+    });
+
+    _config.profile = _imagePath;
   }
 }
