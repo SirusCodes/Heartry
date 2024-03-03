cfa0bfe361bb176d4eabd4e5084ce9d274559489
SirusCodes <hey@darshanrander.com>
2024-03-03 23:00:32 +0530

    feat: made token manager to handle tokens from oauth

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,11 +1,10 @@
 import 'dart:async';
-import 'dart:convert';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:googleapis/drive/v3.dart';
-import 'package:http/http.dart' as http;
+
+import 'token_manager.dart';
 
 const _accessScopes = [DriveApi.driveAppdataScope];
 
@@ -37,19 +36,14 @@ class AuthProvider extends AsyncNotifier<GoogleSignInAccount?> {
     try {
       final account = await _googleSignIn.signIn();
       final canAccess = await _googleSignIn.requestScopes(_accessScopes);
-      if (!canAccess) {
+      if (account == null || !canAccess) {
         state = AsyncAccount.error('Access denied', StackTrace.current);
+        return;
       }
 
-      // final refreshToken = await _getRefreshToken(account!.serverAuthCode!);
-      // final accessToken = (await account.authentication).accessToken;
-
-      // if (refreshToken == null || accessToken == null) {
-      //   state = AsyncAccount.error('Failed to get tokens', StackTrace.current);
-      //   return;
-      // }
-
-      // await _saveTokens(refreshToken, accessToken);
+      await ref
+          .read(tokenManagerProvider)
+          .getAndSaveTokensFromAuthCode(account.serverAuthCode!);
 
       state = AsyncAccount.data(account);
     } catch (e, st) {
@@ -64,67 +58,6 @@ class AuthProvider extends AsyncNotifier<GoogleSignInAccount?> {
     } catch (e, st) {
       state = AsyncAccount.error(e, st);
     }
-  }
-
-  Future<String?> _getRefreshToken(String authCode) async {
-    final url = Uri.parse('https://accounts.google.com/o/oauth2/token');
-
-    final body = {
-      'code': authCode,
-      'client_id': const String.fromEnvironment('GOOGLE_CLIENT_ID'),
-      'client_secret': const String.fromEnvironment('GOOGLE_CLIENT_SECRET'),
-      'redirect_uri': '',
-      'grant_type': 'code',
-      'access_type': 'offline',
-    };
-
-    final response = await http.post(url, body: body);
-
-    final json = jsonDecode(response.body);
-
-    return json['refresh_token'];
-  }
-
-  Future<void> _saveTokens(String refreshToken, String accessToken) {
-    const storage = FlutterSecureStorage(
-      aOptions: AndroidOptions(encryptedSharedPreferences: true),
-    );
-
-    return Future.wait([
-      storage.write(key: REFRESH_TOKEN, value: refreshToken),
-      storage.write(key: ACCESS_TOKEN, value: accessToken),
-    ]);
-  }
-
-  Future<String?> getAccessToken({bool forceRefresh = false}) async {
-    const storage = FlutterSecureStorage(
-      aOptions: AndroidOptions(encryptedSharedPreferences: true),
-    );
-
-    if (!forceRefresh) {
-      return storage.read(key: ACCESS_TOKEN);
-    }
-
-    final newToken = await _getAccessTokenFromServer();
-
-    await storage.write(key: ACCESS_TOKEN, value: newToken);
-
-    return newToken;
-  }
-
-  Future<String> _getAccessTokenFromServer() {
-    final uri = Uri.parse('https://www.googleapis.com/oauth2/v4/token');
-
-    final body = {
-      'client_id': const String.fromEnvironment('GOOGLE_CLIENT_ID'),
-      'client_secret': const String.fromEnvironment('GOOGLE_CLIENT_SECRET'),
-      'refresh_token': '',
-      'grant_type': 'refresh_token',
-    };
-
-    return http
-        .post(uri, body: body)
-        .then((value) => jsonDecode(value.body)['access_token']);
   }
 
   @override
---
+++ lib/providers/token_manager.dart
@@ -0,0 +1,128 @@
+import 'dart:convert';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:googleapis/drive/v3.dart';
+import 'package:http/http.dart' as http;
+
+final tokenManagerProvider = Provider<TokenManager>((_) => TokenManager());
+
+class TokenManager {
+  TokenManager();
+
+  static const storage = FlutterSecureStorage();
+
+  static const String _accessTokenKey = "accessToken",
+      _refreshToken = "refreshToken",
+      _expireDate = "expireDate";
+
+  final Uri _uri = Uri.parse('https://heartry-server.globeapp.dev');
+
+  Future<String> getAccessToken() async {
+    if (!(await _isTokenExpired())) {
+      final accessToken = await storage.read(key: _accessTokenKey);
+      if (accessToken == null) {
+        throw Exception('Access token is null');
+      }
+
+      return accessToken;
+    }
+
+    return _getAccessTokenFromRefreshToken();
+  }
+
+  Future<void> getAndSaveTokensFromAuthCode(String serverAuthCode) async {
+    final response = await http.post(
+      _uri,
+      body: jsonEncode({'code': serverAuthCode}),
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to get tokens from auth code');
+    }
+
+    final json = jsonDecode(response.body);
+    final refreshToken = json['refresh_token'];
+    final accessToken = json['access_token'];
+    final expireDate = DateTime.now().add(
+      Duration(seconds: json['expires_in']),
+    );
+
+    if (json['scope'] != DriveApi.driveAppdataScope) {
+      throw Exception('Invalid scope');
+    }
+
+    await _saveTokensAndExpireDate(
+      refreshToken: refreshToken,
+      accessToken: accessToken,
+      expireDate: expireDate,
+    );
+
+    return;
+  }
+
+  Future<bool> _isTokenExpired() async {
+    final expireDateString = await storage.read(key: _expireDate);
+    if (expireDateString == null) return false;
+
+    final expireDate = DateTime.parse(expireDateString);
+    return expireDate.isBefore(DateTime.now());
+  }
+
+  Future<String> _getAccessTokenFromRefreshToken() async {
+    final refreshToken = await storage.read(key: _refreshToken);
+    if (refreshToken == null) {
+      throw Exception('Refresh token is null');
+    }
+
+    final response = await http.post(
+      _uri,
+      body: jsonEncode({'refresh_token': refreshToken}),
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to get access token from refresh token');
+    }
+
+    final json = jsonDecode(response.body);
+    final accessToken = json['access_token'];
+    final expireDate = DateTime.now().add(
+      Duration(seconds: json['expires_in']),
+    );
+    final scope = json['scope'];
+
+    if (scope != DriveApi.driveAppdataScope) {
+      throw Exception('Invalid scope');
+    }
+
+    await updateAccessToken(accessToken: accessToken, expireDate: expireDate);
+
+    return accessToken;
+  }
+
+  Future<void> _saveTokensAndExpireDate({
+    required String refreshToken,
+    required String accessToken,
+    required DateTime expireDate,
+  }) async {
+    await Future.wait([
+      storage.write(key: _refreshToken, value: refreshToken),
+      storage.write(key: _accessTokenKey, value: accessToken),
+      storage.write(key: _expireDate, value: expireDate.toIso8601String())
+    ]);
+
+    return;
+  }
+
+  Future<void> updateAccessToken({
+    required String accessToken,
+    required DateTime expireDate,
+  }) async {
+    await Future.wait([
+      storage.write(key: _accessTokenKey, value: accessToken),
+      storage.write(key: _expireDate, value: expireDate.toIso8601String())
+    ]);
+
+    return;
+  }
+}
